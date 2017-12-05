26ca5a400f303399f03cb550b4b6cbf038967e3a
laurent.hadjadj <laurent.hadjadj@lilmod-lelamed.fr>
2017-12-05 10:12:00 +0100

    version 1.1
    Ajout de la requete sur les type de produit

--- src/PortailBundle/Controller/PortailController.php
+++ src/PortailBundle/Controller/PortailController.php
@@ -5,6 +5,7 @@ use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\JsonResponse;
+use PDO;
 
 class PortailController extends Controller
 {
@@ -23,18 +24,33 @@ class PortailController extends Controller
      */
     public function carteMenuAction(Request $request)
     {
+	  //Test si la requête est de type AJAX
 	  if ($request->get('categorieSelectionnee')!=null)
 		{
 		 $resultat=$request->get('categorieSelectionnee');
 		 $response = new JsonResponse();
          $response->setData(['resultat'=>$resultat]);
-         return $rsponse;
+         return $response;
 		}
 
+	  // La requêtes n'est de type AJax. Ouverture de la page
 	  $titre="Carte des menus";
 
+	  // j'ouvre la base de données
+	  $db = new PDO("sqlite:/opt/DATA/SQLITE/ok-pizza.db");
+	  // je prépare la requete SQL
+	  $requeteType="select distinct type from produits order by type asc";
+      // j'execute la requête et je retourne les valeurs dans un tableau Asscoiatif
+	  $categorie= $db->query($requeteType)->fetchAll(PDO::FETCH_ASSOC);
+
+	  // je deboggue
+	  //var_dump($resultat);
+
+	  // je debbogue : j'affiche le tableau
+	  //foreach ($resultat as $value) {print_r($value['type']); echo "<br />";}
+
       return $this->render('PortailBundle::carte.html.twig',
-	  ['titre'=>$titre ]);
+	  ['titre'=>$titre, 'categories'=> $categorie]);
 
     }
 
